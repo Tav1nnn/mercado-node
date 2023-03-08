b3ad2eede99bbcca673149e451cbcc7ad8ddf0ff
Otavio Marques <marquesotavio251204@gmail.com>
2023-03-08 19:37:30 -0300

    multer

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,13 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from '../config/multer';
 import midleware from '../middleware/midleware';
 import userCadastro from '../controllers/userController/ControllerCadastro';
 import userLogin from '../controllers/userController/ControllerLogin';
 import produtoCadastro from '../controllers/produtosController/cadastroProdutosController';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/login', userLogin.login);
 routes.post('/cadastro', userCadastro.cadastro);
@@ -12,5 +15,6 @@ routes.post('/cadastro', userCadastro.cadastro);
 routes.use(midleware);
 
 routes.post('/cadastroProduto', produtoCadastro.cadastroProduto);
+routes.post('/file', upload.single('file'), (req, res) => res.json({ response: 'ok' }));
 
 export default routes;
